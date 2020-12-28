2af3ea8feacc3f4ec1a5f2153d077959f2772fe3
Thijs Baaijen <Thijss@users.noreply.github.com>
2020-12-28 14:25:40 +0100

    Update with deploy workflow and lint workflow

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -1,6 +1,9 @@
 """Settings for deploying the project to on Heroku"""
 # pylint: disable=wildcard-import,unused-wildcard-import,relative-beyond-top-level
-import django_heroku
+import os
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 from .base import *  # noqa
 
@@ -15,4 +18,26 @@ SECURE_HSTS_PRELOAD = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-django_heroku.settings(locals())
+ALLOWED_HOSTS = ["*"]
+
+# Ensure STATIC_ROOT exists.
+os.makedirs(STATIC_ROOT, exist_ok=True)
+# Insert Whitenoise Middleware.
+MIDDLEWARE += ["whitenoise.middleware.WhiteNoiseMiddleware"]
+# Enable GZip.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+if SENTRY_URL := env("SENTRY_URL", ""):
+    sentry_sdk.init(
+        dsn=SENTRY_URL,
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
+
+# pylint: disable=fixme
+# TODO: When this is set to True, a Value error is thrown (see Sentry)
+WHITENOISE_MANIFEST_STRICT = False
